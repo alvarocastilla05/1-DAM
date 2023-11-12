8e7e16ff509875d1e4dda014c3202456503d39fa
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2023-11-12 13:30:14 +0100

    Ejercicios Programacion

--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio04/Cilindro.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio04/Cilindro.java
@@ -1,5 +1,29 @@
 package ejercicio04;
 
 public class Cilindro {
+	
+	private double radio;
+	private double altura;
+	private double volumen;
+	
 
+	public Cilindro(double radio, double altura, double volumen) {
+		
+		this.radio = radio;
+		this.altura = altura;
+		this.volumen = volumen;
+	}
+
+	public double calcularVol () {
+		
+		int dos=2;
+		volumen=Math.PI*(Math.pow(radio, dos))*altura;
+		return volumen;
+		
+	}
+	
+	public double mostrarArea() {
+		System.out.printf("El volumen del cilindro es %.2f m3", volumen);
+		return volumen;
+	}
 }
--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio04/Principal.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio04/Principal.java
@@ -1,10 +1,36 @@
 package ejercicio04;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//Declarar variables.
+		
+		double radio;
+		double altura;
+		double volumen = 0;
+		String aux;
+		Cilindro c;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Bienvenido, en el siguiente programa vamos a calcular el volumen de un cilindro");
+		System.out.println("*********************************************************************************");
+		
+		System.out.println("Introduzca el radio del cilindro: ");
+		aux=sc.nextLine();
+		radio=Double.parseDouble(aux);
+		
+		System.out.println("Introduzca la altura del cilindro: ");
+		aux=sc.nextLine();
+		altura=Double.parseDouble(aux);
+		
+		c=new Cilindro(radio, altura, volumen);
+		
+		c.calcularVol();
+		
+		c.mostrarArea();
 	}
 
 }
