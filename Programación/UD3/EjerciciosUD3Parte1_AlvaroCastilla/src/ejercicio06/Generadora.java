afea5c7b2d76c2274ef681c328c193f5b6f379c7
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2023-11-12 18:32:54 +0100

    Intento ejercicio 6 programacion

--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio06/Generadora.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio06/Generadora.java
@@ -1,5 +1,10 @@
 package ejercicio06;
 
+import java.util.Random;
+
 public class Generadora {
 
+	public void generarNum(int num) {
+		Random rnd=new Random(System.nanoTime());
+	}
 }
--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio06/Principal.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio06/Principal.java
@@ -1,10 +1,41 @@
 package ejercicio06;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int op;
+		String aux;
+		
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Bienvenido, en el siguiente programa vamos a crear un generador de números.");
+		System.out.println("******************************************************************************");
+		
+		do {
+			System.out.println("0-Salir");
+			System.out.println("1-1x2");
+			System.out.println("2-Par o Impar");
+			System.out.println("3-Chinos");
+			System.out.println("4-Primitiva");
+			
+			aux=sc.nextLine();
+			op=Integer.parseInt(aux);
+			
+			switch(op) {
+				case 0:
+					System.out.println("Gracias por utilizar este programa. :)");
+					break;
+					
+					
+			}
+			
+		}while(op!=0);
 	}
 
 }
