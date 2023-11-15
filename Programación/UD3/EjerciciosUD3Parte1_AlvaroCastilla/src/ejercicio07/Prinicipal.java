95f1314264677148f0ad605c5cbe280f70a14bbf
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2023-11-15 23:43:11 +0100

    Intento ejercicio07 programacion

--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio07/Prinicipal.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio07/Prinicipal.java
@@ -1,9 +1,40 @@
 package ejercicio07;
 
+import java.util.Scanner;
+
 public class Prinicipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int hasta = 2;
+		int desde = 1;
+		int num=0;
+		int numJug;
+		
+		String aux;
+		Moneda m;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Bienvenido al juego de cara o cruz.");
+		System.out.println("*******************************************");
+		
+		System.out.println();
+		
+		System.out.println("Diga 1 para cara.");
+		System.out.println("Diga 2 para cruz.");
+		aux=sc.nextLine();
+		numJug=Integer.parseInt(aux);
+		
+		m=new Moneda();
+		
+		m.generarNum(num, hasta, desde);
+		m.lanzar(m.generarNum(num, hasta, desde));
+		m.mostrarResult(m.lanzar(num));
+		m.comprobarGan(num, numJug);
+		
+		
 
 	}
 
