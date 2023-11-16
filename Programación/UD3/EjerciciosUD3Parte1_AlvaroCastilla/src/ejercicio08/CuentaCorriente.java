a0c0ae01ad871829596d706df24b669a20d013de
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2023-11-16 23:09:18 +0100

    Ejercico 8 programación completo

--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio08/CuentaCorriente.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio08/CuentaCorriente.java
@@ -10,13 +10,36 @@ public class CuentaCorriente {
 		this.nombre = nombre;
 	}
 
-	public double retirarCant (double saldo) {
-		double retiro;
+	public double retirarCant (double retiro) {
 		double saldoRetiro;
 		
-		saldoRetiro=saldo;
+		saldoRetiro=saldo-retiro;
 		return saldoRetiro;
 		
+	}
+	public void mostrarRetiro(double saldoRetiro) {
+		System.out.printf("Su saldo actual es %.2f €\n", saldoRetiro);
+	}
+	
+	public double ingresarCant (double ingreso) {
+		double saldoIngreso;
+		
+		saldoIngreso=saldo+ingreso;
+		return saldoIngreso;
+		
+	}
+	public void mostrarIngreso(double saldoIngreso) {
+		System.out.printf("Su saldo actual es %.2f €\n", saldoIngreso);
+	}
+	
+	public double calcularDolares (double dolares) {
+		double tasaEurDol=1.15;
+		dolares= tasaEurDol*saldo;
+		
+		return dolares;
+	}
+	public void mostrarDolares(double dolares) {
+		System.out.printf("Tienes un total de %.2f $\n", dolares);
 	}
 		
 	
--- Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio08/Principal.java
+++ Programación/UD3/EjerciciosUD3Parte1_AlvaroCastilla/src/ejercicio08/Principal.java
@@ -1,5 +1,7 @@
 package ejercicio08;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -7,7 +9,64 @@ public class Principal {
 
 		double saldo;
 		String nombre;
-		double saldoRetiro;
+		double ingreso;
+		double retiro;
+		double dolares = 0;
+		String aux;
+		int op;
+		CuentaCorriente c;
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Bienvenido al progama de ingreso y retiro de dinero.");
+		System.out.println("********************************************************");
+		System.out.println("  ");
+		
+		System.out.println("Introduzca su nombre:");
+		nombre=sc.nextLine();
+		System.out.println("Indique su saldo:");
+		aux=sc.nextLine();
+		saldo=Double.parseDouble(aux);
+		System.out.println("  ");
+		
+		c=new CuentaCorriente(saldo, nombre);
+		
+		do {
+			System.out.println("-------------");
+			System.out.println("0-Salir.");
+			System.out.println("1-Retirar Dinero.");
+			System.out.println("2-Ingresar Dinero.");
+			System.out.println("3-Calcular dólares americanos.");
+			System.out.println("4-Consultar el saldo.");
+			System.out.println("-------------");
+			
+			aux=sc.nextLine();
+			op=Integer.parseInt(aux);
+			
+			switch(op) {
+				case 1:
+					System.out.println("Ingrese la cantidad que desea retirar:");
+					aux=sc.nextLine();
+					retiro=Double.parseDouble(aux);
+			
+					c.mostrarRetiro(c.retirarCant(retiro));
+					break;
+				case 2:
+					System.out.println("Ingrese la cantidad que desea ingresar:");
+					aux=sc.nextLine();
+					ingreso=Double.parseDouble(aux);
+					
+					c.mostrarIngreso(c.ingresarCant(ingreso));
+					break;
+				case 3:
+					c.mostrarDolares(c.calcularDolares(dolares));
+			}
+			
+			
+		}while(op!=0);
+		
+		
 
 	}
 
