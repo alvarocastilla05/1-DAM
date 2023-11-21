344f469d2ab5b43b8a85a664a52758446601a8a9
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2023-11-21 12:31:56 +0100

    Corrección BD

--- ED y BD/pgadmin/UD4/ACTIVIDAD3.sql
+++ ED y BD/pgadmin/UD4/ACTIVIDAD3.sql
@@ -42,9 +42,13 @@ WHERE provincia IN ('Cádiz', 'Huelva', 'Málaga', 'Almería', 'Granada')
 	AND tipo_operacion = 'Alquiler';
 	
 --Corregido.
-SELECT *
-FROM inmueble JOIN tipo ON (tipo_inmuble = id_tipo)
+SELECT ROUND(avg(precio/superficie),2) AS mediaM2
+FROM inmueble JOIN tipo ON (tipo_inmueble = id_tipo)
 WHERE EXTRACT( MONTH from fecha_alta) IN (3,4)	
+	AND provincia IN ('Huelva', 'Cádiz', 'Málaga','Almería', 'Granada')
+	AND nombre IN ('Casa','Piso')
+	AND tipo_operacion = 'Alquiler';
+
 /*3. ¿Cuál es la media del porcentaje de diferencia entre el precio inicial 
 (en la tabla inmueble) y el precio final (en la tabla operación)
 para aquellas operaciones de alquiler realizadas en enero de cualquier año, 
@@ -54,8 +58,16 @@ SELECT ROUND(avg(precio/precio_final*100),2)
 FROM inmueble i JOIN operacion USING (id_inmueble)
 	JOIN tipo ON (tipo_inmueble=id_tipo)
 WHERE  TO_CHAR(fecha_operacion, 'MM') = '01'
- AND nombre IN ('Oficina', 'Local', 'Suelo');
+	AND nombre IN ('Oficina', 'Local', 'Suelo');
  
+--Corregido.
+SELECT ROUND(AVG((ABS(precio-precio_final)/precio)*100),2)  --Valor absoluto (ABS)
+FROM inmueble JOIN operacion USING (id_inmueble)
+			JOIN tipo ON (tipo_inmueble=id_tipo)
+WHERE nombre IN ('Oficina', 'Local', 'Suelo')
+	AND TO_CHAR(fecha_operacion, 'MM') = '01'
+	AND tipo_operacion = 'Alquiler';
+
  
 /*4. Seleccionar el nombre de aquellos compradores de Casa o Piso en las provincias de Jaén o 
 Córdoba, donde el precio final de inmueble esté entre 150.000 y 200.000€, para aquellos 
@@ -64,12 +76,26 @@ inmuebles que han tardado entre 3 y 4 meses en venderse.*/
 SELECT c.nombre
 FROM comprador c JOIN operacion USING (id_cliente)
 	JOIN inmueble USING (id_inmueble)
-	JOIN tipo t USING (id_inmueble)
+	JOIN tipo t ON (tipo_inmueble=id_tipo)
 WHERE t.nombre IN ('Casa', 'Piso')
 	AND provincia IN ('Jaén', 'Córdoba')
 	AND precio BETWEEN 150000 AND 200000
-	AND (fecha_operacion-fecha_alta) BETWEEN 90 AND 120;
+	AND (fecha_operacion-fecha_alta) BETWEEN 90 AND 120; --Esta línea está MAL.
 	
+--Corregido.
+SELECT c.nombre 
+FROM comprador c JOIN operacion USING (id_cliente)
+	JOIN inmueble USING (id_inmueble)
+	JOIN tipo t ON (tipo_inmueble=id_tipo)
+WHERE t.nombre IN ('Casa', 'Piso')
+	AND provincia IN ('Jaén','Córdoba')
+	AND tipo_operacion = 'Venta'
+	AND precio_final BETWEEN 150000 AND 200000
+	--AND AGE(fecha_operacion, fecha_alta) BETWEEN '3 mon'::interval AND '4 mon'::interval; --AGE(mayor,menor).
+	AND fecha_operacion BETWEEN 
+		fecha_alta + '3 mons'::interval AND 
+		fecha_alta + '4 mons'::interval;--Otra opción para la fecha.
+		
 /*5. Selecciona la media del precio inicial (en la tabla inmueble), del precio final
 (en la tabla operación) y de la diferencia en porcentaje entre ellas de aquellas viviendas 
 (Casa o Piso) en alquiler que tengan menos de 100 metros cuadrados y que hayan tardado un año o
@@ -81,8 +107,18 @@ SELECT ROUND(avg (precio),2) ,
 FROM inmueble  JOIN operacion  USING (id_inmueble)
         JOIN tipo  ON (tipo_inmueble = id_tipo)
 WHERE  nombre IN ('Casa','Piso')
-  AND superficie > 100
-  AND fecha_operacion-fecha_alta >= 365;
+	AND superficie > 100
+	AND fecha_operacion-fecha_alta >= 365;
+  
+--Corregido.
+SELECT AVG(precio), AVG(precio_final)
+FROM inmueble JOIN tipo ON (tipo_inmueble=id_tipo)
+	JOIN operacion USING (id_inmueble)
+WHERE nombre IN ('Casa','Piso')
+	AND superficie < 100 
+	AND tipo_operacion = 'Alquiler'
+	AND fecha_operacion >=
+	fecha_alta + '1 year'::interval;
 
 
 
