47ee95a944670b7aca1de661f257229fdcdcda13
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2023-11-28 12:35:38 +0100

    Correccion BD

--- ED y BD/pgadmin/UD4/ACTIVIDAD6.sql
+++ ED y BD/pgadmin/UD4/ACTIVIDAD6.sql
@@ -12,25 +12,65 @@ WHERE nombre = 'Piso'
 ORDER BY precio DESC
 LIMIT 3;
 
+--Corregido (Hemos puesto otra fecha para comprobar que está bien, ya que en 2019 no hay nada)
+SELECT *
+FROM tipo JOIN inmueble ON (id_tipo=tipo_inmueble) 
+	JOIN operacion USING (id_inmueble)
+WHERE nombre = 'Piso'
+	AND TO_CHAR(fecha_operacion, 'YYYY') = '2021'
+	AND tipo_operacion = 'Venta'
+	AND provincia = 'Sevilla'
+ORDER BY precio DESC
+LIMIT 3;
+
 /*2. Selecciona el precio medio de los alquileres en Málaga en los meses de Julio y 
 Agosto (da igual de qué año).*/
 
 SELECT avg(precio_final)
-FROM inmueble JOIN tipo ON (tipo_inmueble=id_tipo)
-	JOIN operacion USING (id_inmueble)
+FROM inmueble JOIN operacion USING (id_inmueble)
 WHERE provincia = 'Málaga'
-	AND TO_CHAR(fecha_operacion, 'MM') IN ('07', '08');
+	AND TO_CHAR(fecha_operacion, 'MM') IN ('07', '08')
+	AND tipo_operacion = 'Alquiler';
 	
+--Corregido (Cambiamos fecha_operacion por fecha_alta ya que no especifica si está alquilada ya o no)
+SELECT avg(precio_final)
+FROM inmueble JOIN operacion USING (id_inmueble)
+WHERE provincia = 'Málaga'
+	AND TO_CHAR(fecha_alta, 'MM') IN ('07', '08')
+	AND tipo_operacion = 'Alquiler';	
+
+--Realizamos la misma ACT pero indicando los inmuebles que aún no se hayan alquilado
+
+SELECT *
+FROM inmueble LEFT JOIN operacion o USING (id_inmueble)
+WHERE provincia = 'Málaga'
+	AND TO_CHAR(fecha_alta, 'MM') IN ('07', '08')
+	AND tipo_operacion = 'Alquiler'
+	AND o.id_inmueble IS NULL;
+
 /*3. Selecciona los inmuebles que se han vendido en Jaén y Córdoba en los últimos 3 meses 
 del año 2019 o 2020.*/
 
-
+--(Cambié Q por 4 porque dice los últimos 3 meses, es decir, el 4º trimestre del año).
 SELECT *
 FROM inmueble JOIN operacion USING (id_inmueble)
 WHERE provincia IN ('Jaén', 'Córdoba')
 	AND tipo_operacion = 'Venta'
-	AND TO_CHAR(fecha_operacion, 'Q') = '3'
-	AND TO_CHAR(fecha_operacion, 'YYYY') IN ('2019', '2020');
+	AND TO_CHAR(fecha_operacion, 'Q') = '4'
+	AND TO_CHAR(fecha_operacion, 'YYYY') IN ('2021', '2022');
+
+--Corregido(Está bien)
+
+--Hacer lo mismo pero que se haya vendido del 15 de enero al 15 de marzo*/
+SELECT *
+FROM inmueble JOIN operacion USING (id_inmueble)
+WHERE provincia IN ('Jaén', 'Córdoba')
+	AND tipo_operacion = 'Venta'
+	AND TO_CHAR(fecha_operacion, 'MM-DD') BETWEEN '01-15' AND '03-15';
+	
+--Corregido por Luismi en classroom.
+	
+
 	
 /*4. Selecciona el precio medio de las ventas de Parking en la provincia de Huelva para 
 aquellas operaciones que se realizaran entre semana (de Lunes a Viernes).*/
@@ -50,6 +90,8 @@ SELECT *
 FROM tipo JOIN inmueble ON (tipo_inmueble=id_tipo)
 	JOIN operacion USING (id_inmueble)
 WHERE provincia = 'Granada'
+	AND tipo_operacion = 'Venta'
+	AND nombre = 'Piso'
 	AND AGE(fecha_operacion, fecha_alta) BETWEEN '3 month'::interval
 											AND '6 month'::interval;
 
