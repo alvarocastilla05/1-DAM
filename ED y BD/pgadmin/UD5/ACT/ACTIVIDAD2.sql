f92e15ca51e6e051e9ccc23cdfe22ed2ef9d7f9a
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2023-12-11 13:31:41 +0100

    Comienzco ACT BD

--- ED y BD/pgadmin/UD5/ACT/ACTIVIDAD2.sql
+++ ED y BD/pgadmin/UD5/ACT/ACTIVIDAD2.sql
@@ -8,7 +8,35 @@ HAVING COUNT(*) BETWEEN 100 AND 150;
 
 /*2. Seleccionar el nombre de las empresas que no han realizado ningún pedido.*/
 
-SELECT company_name, COUNT(*)
-FROM shippers LEFT JOIN orders ON (ship_via = shipper_id)
-GROUP BY company_name
+SELECT company_name
+FROM customers LEFT JOIN orders USING (customer_id)
+WHERE order_id IS NULL
+GROUP BY company_name;
+
+/*3. Seleccionar la categoría que tiene más productos diferentes solicitados en pedidos. 
+Mostrar el nombre de la categoría y dicho número.*/
+
+SELECT category_name, category_id, COUNT(*)
+FROM categories JOIN products USING (category_id)
+	JOIN order_details USING (product_id)
+	JOIN orders USING (order_id)
+GROUP BY category_name, category_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+/*4. Si suponemos que nuestro margen de beneficio con los productos es de un 25% (es decir, el 25% de su precio, 
+son beneficios, y el 75% son costes), calcular la cantidad de beneficio que hemos obtenido, agrupado por categoría y
+producto. Las cantidades deben redondearse a dos decimales.*/
+
+SELECT (unit_price*units_on_order*0.25) as "beneficios"
+FROM products JOIN order_details USING (product_id)
+	JOIN orders USING (order_id)
+
+
+
+
+/*5. Selecciona aquellos clientes (CUSTOMERS) para los que todos los envíos que ha recibido (sí, todos)
+los haya transportado (SHIPPERS) la empresa United Package.*/
+
+
 
