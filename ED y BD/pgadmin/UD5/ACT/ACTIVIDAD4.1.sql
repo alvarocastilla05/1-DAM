0ccd74729d790a308d7a6d50dd1a4ce4ec2c48a3
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2023-12-15 09:55:33 +0100

    Correccion de BD

--- ED y BD/pgadmin/UD5/ACT/ACTIVIDAD4.1.sql
+++ ED y BD/pgadmin/UD5/ACT/ACTIVIDAD4.1.sql
@@ -14,16 +14,50 @@ WHERE tipo.nombre = 'Piso'
 			JOIN tipo ON (tipo_inmueble=id_tipo)
 		WHERE tipo.nombre = 'Piso'
 			AND tipo_operacion = 'Venta'
-	)
+	);
+	
+--Corregido.
 
+SELECT provincia, comprador.nombre, fecha_operacion, precio_final
+FROM operacion JOIN inmueble i1 USING (id_inmueble)
+	JOIN tipo ON (tipo_inmueble=id_tipo)
+	JOIN comprador USING (id_cliente)
+WHERE tipo.nombre = 'Piso'
+	AND tipo_operacion = 'Venta'
+	AND precio_final >= ALL (
+		SELECT precio_final
+		FROM operacion JOIN inmueble i2 USING (id_inmueble)
+			JOIN tipo ON (tipo_inmueble=id_tipo)
+			JOIN comprador USING (id_cliente)
+		WHERE tipo.nombre = 'Piso'
+			AND tipo_operacion = 'Venta'
+			AND i1.provincia = i2.provincia
+);
+	
 /*4.Seleccionar los alquileres más baratos de cada provincia y mes (da igual el día y el año). 
 Debe aparecer el nombre de la provincia, el nombre del mes, el resto de atributos de la tabla inmueble y el 
 precio final del alquiler.*/
 
 SELECT provincia, TO_CHAR(fecha_operacion, 'Month'), 
-FROM operacion JOIN inmueble USING (id_inmueble)
+FROM operacion JOIN inmueble i USING (id_inmueble)
 	JOIN tipo ON (tipo_inmueble=id_tipo)
 
+
+--Corregido.
+
+SELECT provincia, TO_CHAR(fecha_operacion, 'Month'), id_inmueble,
+	fecha_alta, tipo_inmueble, tipo_operacion, superficie, precio
+FROM inmueble i1 JOIN operacion o1 USING (id_inmueble)
+WHERE tipo_operacion = 'Alquiler'
+	AND precio_final >= ALL (
+			SELECT precio_final
+			FROM inmueble i2 JOIN operacion o2 USING (id_inmueble)
+			WHERE tipo_operacion = 'Alquiler'
+				AND i1.provincia = i2.provincia
+				AND EXTRACT(month from o1.fecha_operacion) =
+					EXTRACT(month from o2.fecha_operacion)
+	)
+ORDER BY provincia, EXTRACT(month from o1.fecha_operacion);
 	
 	
 
--- ED y BD/pgadmin/UD5/ACT/ACTIVIDAD4.sql
+++ ED y BD/pgadmin/UD5/ACT/ACTIVIDAD4.sql
@@ -10,6 +10,29 @@ WHERE AGE(llegada, salida) >= ALL(
 	GROUP BY dia, duracion
 )
 
+--Corregido.
+
+SELECT o.ciudad, salida, TO_CHAR(salida, 'dy'), d.ciudad, llegada,
+	AGE(llegada, salida)
+FROM vuelo v1 JOIN aeropuerto o ON (desde=o.id_aeropuerto)
+	JOIN aeropuerto d ON (hasta=d.id_aeropuerto)
+WHERE age(llegada, salida) >= ALL (
+			SELECT age(llegada, salida)
+			FROM vuelo v2
+			WHERE EXTRACT(isodow from v1.salida)
+				= EXTRACT(isodow from v2.salida)
+)
+ORDER BY EXTRACT(isodow from v1.salida);
+
+--Seleccionar la duracion max por dia de la semana de los vuelos.
+
+SELECT TO_CHAR(salida, 'dy'), MAX(age(llegada, salida))
+FROM vuelo
+GROUP BY TO_CHAR(salida, 'dy');
+
+--Si hay que mostrar algun dato que se muestra en las filas individuales, no podemos hacer GROUP BY.
+
+
 /*2. Seleccionar el cliente que más ha gastado en vuelos que salen del mismo aeropuerto. Debe aparecer el nombre del cliente, 
 el nombre y la ciudad del aeropuerto y la cuantía de dinero que ha gastado.*/
 
@@ -23,4 +46,19 @@ GROUP BY c.nombre, a.nombre, a.ciudad
 ORDER BY DineroGastado DESC
 LIMIT 1;
 
+--Corregido.
 
+SELECT c.nombre, c.apellido1, c.apellido2, a.nombre, a.ciudad,
+	SUM(precio * (1-(COALESCE(descuento,0)/100)))
+FROM vuelo v1 JOIN reserva USING (id_vuelo)
+	JOIN cliente c USING (id_cliente)
+	JOIN aeropuerto a ON (desde=id_aeropuerto)
+GROUP BY c.nombre, c.apellido1, c.apellido2, a.nombre, a.ciudad, v1.desde
+HAVING SUM(
+	precio * (1-(COALESCE(descuento,0)/100)))
+	>= ALL (
+		SELECT SUM(precio * (1-(COALESCE(descuento,0)/100)))
+		FROM vuelo v2 JOIN reserva USING (id_vuelo)
+		WHERE v1.desde = v2.desde
+		GROUP BY id_cliente
+	)
