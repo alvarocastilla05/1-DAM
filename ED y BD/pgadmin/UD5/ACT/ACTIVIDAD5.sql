3e148547eac61348cc20d740d0e17aa18726f217
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2023-12-17 23:04:50 +0100

    Ejercicios de BD

--- ED y BD/pgadmin/UD5/ACT/ACTIVIDAD5.sql
+++ ED y BD/pgadmin/UD5/ACT/ACTIVIDAD5.sql
@@ -29,3 +29,42 @@ FROM cliente JOIN reserva USING (id_cliente)
 	JOIN aeropuerto d ON (desde=d.id_aeropuerto)
 	JOIN aeropuerto l ON (hasta=l.id_aeropuerto)
 GROUP BY id_cliente,  TO_CHAR(salida, 'Month'), TO_CHAR(llegada, 'Month'), d.nombre, l.nombre
+
+/*3. Suponiendo que el 30% del precio de cada billete son beneficios (y el 70% son gastos), 
+¿cuál es el trayecto que más rendimiento económico da? Es decir, ¿en qué trayecto estamos ganando más dinero? 
+¿Y con el que menos? Lo puedes hacer en consultas diferentes usando WITH*/
+
+WITH por_beneficios AS (
+		SELECT (precio*(1-COALESCE(descuento,0)/100)) as "beneficios", o.nombre, l.nombre
+		FROM vuelo JOIN aeropuerto o ON (desde=o.id_aeropuerto)
+			JOIN aeropuerto l ON (hasta=l.id_aeropuerto)
+)
+SELECT beneficios, o.nombre, l.nombre
+FROM por_beneficios
+WHERE beneficios = (SELECT MAX(beneficios) FROM por_beneficios)
+   OR beneficios = (SELECT MIN(beneficios) FROM por_beneficios);
+   
+/*4. Seleccionar el nombre y apellidos de los clientes que no han hecho ninguna reserva para un vuelo que salga
+en el tercer trimestre desde Sevilla.*/
+
+SELECT c.nombre, apellido1, apellido2
+FROM cliente c LEFT JOIN reserva USING (id_cliente)
+	JOIN vuelo USING (id_vuelo)
+	JOIN aeropuerto o ON (desde=o.id_aeropuerto)
+	JOIN aeropuerto l ON (hasta=l.id_aeropuerto)
+WHERE TO_CHAR(salida, 'Q')= '3'
+	AND o.ciudad = 'Sevilla'
+	AND fecha_reserva IS NULL;
+
+
+	
+
+
+
+
+
+
+
+
+
+
