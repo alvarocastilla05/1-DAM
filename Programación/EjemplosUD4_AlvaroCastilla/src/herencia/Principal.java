98ab528e2fc2a9844e207e591f8d855344ddbf9e
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-01-08 12:42:17 +0100

    Programacion

--- Programación/EjemplosUD4_AlvaroCastilla/src/herencia/Principal.java
+++ Programación/EjemplosUD4_AlvaroCastilla/src/herencia/Principal.java
@@ -3,8 +3,21 @@ package herencia;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Trabajador t = new Trabajador("Alvaro", "Programador Junior", "17477928K");
+		Empleado e =new Empleado("Alberto", "Jefe", "295386256W", 2000, 15.0);
+		Consultor c = new Consultor("Miguel", "Experto", "4D", 20, 18);
 
+		
+		Empleado e2 = new Empleado("3C", 1500.0, 12.5);
+		
+		System.out.println(t);
+		System.out.println(e);
+		System.out.println(e2);
+		
+		System.out.println(t.calcularPaga());
+		System.out.println(e.calcularPaga());
+		System.out.println(c.calcularPaga());
 	}
 
 }
