274b90113e236d9e5f0e22516b9f451f78567ccc
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-01-22 09:58:37 +0100

    Correccion ACT5 BD

--- ED y BD/BD/pgadmin/UD6/ACT/ACTIVIDAD5.sql
+++ ED y BD/BD/pgadmin/UD6/ACT/ACTIVIDAD5.sql
@@ -6,7 +6,7 @@ CREATE TABLE alumnos (
 	nombre 		VARCHAR(150) NOT NULL,
 	telefono	VARCHAR(50),
 	edad		INTEGER,
-	empresa 	VARCHAR(200),
+	empresa 	VARCHAR(200), --Deberia de ser 10, igual que el cif ya que al ser clave externa, deben de tener el mismo número de caracteres.
 	CONSTRAINT pk_alumnos PRIMARY KEY (dni)
 );
 
@@ -72,26 +72,26 @@ ALTER TABLE cursos
 ADD CONSTRAINT fk_cursos_profesores FOREIGN KEY (dni_profesor) REFERENCES profesores(dni),
 ADD CONSTRAINT fk_cursos_tipos_cursos FOREIGN KEY (tipo_curso) REFERENCES tipos_curso(cod_curso);
 
-INSERT INTO empresas
+INSERT INTO empresas (cif, nombre, direccion, telefono)
 VALUES ('B14507610', 'Salesianos', 'c/Condes del Bustillo', '954331488'),
 ('C12345678', 'Persianas Jose Luis', 'c/Antonio Guerra Gonzalez 7', '675314070'),
 ('D87456123', 'Bar Paco', 'c/Merinos 8', '659883212');
 
-INSERT INTO alumnos 
+INSERT INTO alumnos (dni, nombre, direccion, telefono, edad, empresa)
 VALUES ('12345678k', 'Alvaro', 'c/Antonio 6', '123456789', 15, 'B14507610'),
 	('54789632K', 'Carlos', 'c/Paco 6', '456789123', 20, 'B14507610'),
 	('98765412F', 'Jose', 'c/Nueva 6', '369852741', 18, 'B14507610'),
 	('74185296H', 'Paco', 'c/Fede 6', '95135746', 21, 'B14507610'),
 	('56435751J', 'Alvaro', 'c/Bustillo 7', '456936879', 50, 'B14507610');
 	
-INSERT INTO alumnos 
+INSERT INTO alumnos (dni, nombre, direccion, telefono, edad, empresa)
 VALUES ('45687921H', 'Alvaro', 'c/Antonio 6', '123456789', 15, 'C12345678'),
 	('59187962J', 'Carlos', 'c/Paco 6', '456789123', 20, 'C12345678'),
 	('75354698M', 'Jose', 'c/Nueva 6', '369852741', 18, 'C12345678'),
 	('39658247I', 'Paco', 'c/Fede 6', '95135746', 21, 'C12345678'),
 	('549251Y', 'Alvaro', 'c/Bustillo 7', '456936879', 50, 'C12345678');
 	
-INSERT INTO alumnos 
+INSERT INTO alumnos (dni, nombre, direccion, telefono, edad, empresa)
 VALUES ('489152P', 'Alvaro', 'c/Antonio 6', '123456789', 15, 'D87456123'),
 	('16587325K', 'Carlos', 'c/Paco 6', '456789123', 20, 'D87456123'),
 	('56435759Y', 'Jose', 'c/Nueva 6', '369852741', 18, 'D87456123'),
