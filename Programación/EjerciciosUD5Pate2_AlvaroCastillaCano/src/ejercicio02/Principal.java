55eec631d6a1d8b8c1b1790e29065553e2d5aec7
Álvaro Castilla Cano <castilla.caalv24@triana.salesianos.edu>
2024-03-13 10:54:47 +0100

    Cosas

--- Programación/EjerciciosUD5Pate2_AlvaroCastillaCano/src/ejercicio02/Principal.java
+++ Programación/EjerciciosUD5Pate2_AlvaroCastillaCano/src/ejercicio02/Principal.java
@@ -1,10 +1,30 @@
 package ejercicio02;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String aux;
+		int op;
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Bienvenido al ejercicio2");
+		System.out.println("-".repeat(100));
+		
+		do {
+		
+			System.out.println("""
+					0-Salir.
+					1-
+					""");
+			aux=sc.nextLine();
+			op=Integer.parseInt(aux);
+		}while(op!=0);
+		
 	}
 
 }
