c24eb68d2cad4cc1ddb2a581ece800752ebf8bde
AlvaroCastilla <castilla.caalv24@triana.salesianos.edu>
2024-04-22 18:03:19 +0200

    Ejercicios de JavaScript

---
+++ HTML/UD9/BoletinUD9/js/ejercicio08.js
@@ -0,0 +1,41 @@
+//Cada vez que cambie algun texto, se pase a mayuscula (menos la contraseña)
+let formulario = document.forms.registro;
+
+for(let campo of formulario.elements){
+    if(campo.type==="text"){
+        campo.addEventListener('input', pasarMayusculas)
+    }else if(campo.type==="password"){
+        campo.nextElementSibling.addEventListener('pointerover', verContrasenia);
+        campo.nextElementSibling.addEventListener('pointerout', ocultarContrasenia);
+    }
+}
+
+let botonEnviar = document.querySelectorAll("button");
+
+botonEnviar.addEventListener("click", validarFormulario);
+
+function pasarMayusculas(elEvento){
+    this.value = this.value.toUpperCase();
+}
+
+function verContrasenia(elEvento){
+    this.previousElementsSibling.type="text";
+}
+
+function ocultarContrasenia(elEvento){
+    this.previousElementsSibling.type="password";
+}
+function validarFormulario(elEvento){
+    let resultado = true;
+    if(formulario.elements.nombre.value==''){
+        resultado = false;
+    }
+    if(!formulario.elements.genero.checked){
+        resultado = false
+    }
+
+    if(resultado){
+        formulario.submit();
+    }
+}
+
